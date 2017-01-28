858b9a19d7b4d8f954baf462ce5b8604d5cc5e16
piekvossen <piek.vossen@vu.nl>
2017-01-28 16:19:51 +0100

    1. dark-entities get a hierarchy by mapping the types from the Named-Entity-Recognition modules (NERC) to the standard hierarchy (dbpedia by default) @DONE @TODO: querying over these types does not yield Dark entities yet. We first need to do the same in the NAF2SEM conversion or the NERC 2. light, dark, concepts blijven drie json files @DONE 3. https://owl/thing wordt verwijderd @DONE 4. authors en cited krijgen ook een hierarchie @DONE @TODO querying over types still needs to be implemented 7. getoverview functies krijgen parameter —out + pad naar result file @DONE

--- scripts/getoverview.sh
+++ scripts/getoverview.sh
@@ -7,11 +7,13 @@ RESOURCES="$( cd $ROOT && cd .. && pwd)"/vua-resources
 # assumes vua-resources is installed next to this installation
 # git clone https://github.com/cltl/vua-resources.git
 
+#KnowledgeStore server address and credentials if needed
 SERVER="http://130.37.53.35:50053"
 KS="nwr/wikinews-new"
 KSUSER="wikinews"
 KSPASS="wikinews"
 
+#Resources needed to build hierarchies
 fnPath="$RESOURCES/frAllRelation.xml"
 esoPath="$RESOURCES/ESO_Version2.owl"
 euroVocLabelFile="$RESOURCES/mapping_eurovoc_skos.label.concept.gz"
@@ -35,28 +37,28 @@ MCOUNT=0;
 
 #OPTIONAL parameter --debug (writes JSON to file for debugging to a file and prints debug information to standard out)
 #OPTIONAL parameter --out to specify the output file
-#OPTIONAL parameter --mention (takes a counter as argument that restricts the phrases and concepts to those that are mentioned more than this threshold
+#OPTIONAL parameter --mention (takes a counter as argument that restricts the phrases and concepts to those that are mentioned equal or more than this threshold
 
-# generate a JSON output stream for ligh-entities using the DBPedia ontology, --entity-hiearchy parameter expects a text file with parent<TAB>child on separate lines.
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --entity-hierarchy $entityHierarchyFile --data "light-entities" --mention $MCOUNT
+# generate a JSON output file or stream for ligh-entities using the DBPedia ontology, --entity-hierarchy parameter expects a text file with parent<TAB>child on separate lines.
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --entity-hierarchy $entityHierarchyFile --data "light-entities" --mention $MCOUNT --out light.json
 
-# generate a JSON output stream for dark-entities using the ENTITY classes assigned to entities in the NAF file. The ENTITY classes are mapped to DBPedia classes through mappings provided by the entityCustomFile
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --data "dark-entities"  --entity-hierarchy $entityHierarchyFile --entity-custom $entityCustomFile --mention $MCOUNT
+# generate a JSON output file or stream for dark-entities using the ENTITY classes assigned to entities in the NAF file.
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --data "dark-entities"   --entity-hierarchy $entityHierarchyFile --entity-custom $entityCustomFile --mention $MCOUNT --out dark.json
 
-# generate a JSON output stream for concepts. Concepts are anything that is not an entity but plays an important role in an event. It uses the DBPedia ontology, --entity-hiearchy parameter expects a text file with parent<TAB>child on separate lines, --entity-type expects a gz file with  <dbpedia resource uri><TAB><dbpedia ontology uri>
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER  --entity-hierarchy $entityHierarchyFile --entity-type $entityTypeFile --data "concepts" --mention $MCOUNT
+# generate a JSON output file or stream for concepts. Concepts are anything that is not an entity but plays an important role in an event. It uses the DBPedia ontology, --entity-hierarchy parameter expects a text file with parent<TAB>child on separate lines, --entity-type expects a gz file with  <dbpedia resource uri><TAB><dbpedia ontology uri>
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --entity-hierarchy $entityHierarchyFile --entity-type $entityTypeFile --data "concepts" --mention $MCOUNT --out concept.json
 
-# generate a JSON output stream for events. It uses two ontologies for building up the hiearchy --eso $esoPath --framenet $fnPath. Standard mode only outputs events mapped to ESO or FrameNet. Use the optional parameter --all-events to also get other events not mapped to ESO or FrameNet
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER  --eso $esoPath --framenet $fnPath --data "events" --mention $MCOUNT
+# generate a JSON output file or stream for events. It uses two ontologies for building up the hierarchy --eso $esoPath --framenet $fnPath. Standard mode only outputs events mapped to ESO or FrameNet. Use the optional parameter --all-events to also get other events not mapped to ESO or FrameNet
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --eso $esoPath --framenet $fnPath --data "events" --out events.json
 
-# generate a JSON output stream for topics.
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER  --eurovoc-label $euroVocLabelFile --eurovoc-core $euroVocHierarchyFile --data "topics"
+# generate a JSON output file or stream for topics.
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --eurovoc-label $euroVocLabelFile --eurovoc-core $euroVocHierarchyFile --data "topics" --mention $MCOUNT --out topics.json
 
-# generate a JSON output stream for authors of the sources (documents). Flat list with countings.
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER  --data "authors"
+# generate a JSON output file or stream for authors of the sources (documents). Flat list with countings.
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --data "authors" --out authors.json
 
-# generate a JSON output stream for cited sources within the documents. Flat list with countings.
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER  --data "cited"
+# generate a JSON output file or stream for cited sources within the documents. Flat list with countings.
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --data "cited" --out cited.json
 
-# generate a JSON output stream for perspective values. Flat list with countings.
-java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER  --data "perspectives"
+# generate a JSON output file or stream for perspective values. Flat list with countings.
+java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonQueryHierarchy --ks-service $SERVER --data "perspectives" --out perspectives.json
